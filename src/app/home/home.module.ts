ac8cab35f42b4ce6acc6af0061fced566d56c137
Daniel <darknessbg@windowslive.com>
2020-06-21 23:19:59 +0300

    init shared module, scrolldown component, added scrolldown to homepage

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from "@angular/common";
 import { HomePageComponent } from "./home-page/home-page.component";
 import { HomeRoutingModule } from "./home.routing.module";
 import { CoreModule } from "../core/core.module";
+import { SharedModule } from "../shared/shared.module";
 
 @NgModule({
   declarations: [HomePageComponent],
-  imports: [CommonModule, HomeRoutingModule, CoreModule],
+  imports: [CommonModule, HomeRoutingModule, CoreModule, SharedModule],
 })
 export class HomeModule {}
---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { ScrollDownComponent } from "./scroll-down/scroll-down.component";
+
+@NgModule({
+  declarations: [ScrollDownComponent],
+  imports: [CommonModule],
+  exports: [ScrollDownComponent],
+})
+export class SharedModule {}
