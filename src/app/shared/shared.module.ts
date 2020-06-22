16073efe8fe1ea898f9aafab14ace56f9e5c9c30
Daniel <darknessbg@windowslive.com>
2020-06-22 22:56:57 +0300

    navbar moved to app htm from homepage, homepage is finished

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,10 +4,21 @@ import { ScrollDownComponent } from "./scroll-down/scroll-down.component";
 import { CardComponent } from "./card/card.component";
 import { ProjectCardComponent } from "./project-card/project-card.component";
 import { RouterModule } from "@angular/router";
+import { FooterComponent } from "./footer/footer.component";
 
 @NgModule({
-  declarations: [ScrollDownComponent, CardComponent, ProjectCardComponent],
+  declarations: [
+    ScrollDownComponent,
+    CardComponent,
+    ProjectCardComponent,
+    FooterComponent,
+  ],
   imports: [CommonModule, RouterModule],
-  exports: [ScrollDownComponent, CardComponent, ProjectCardComponent],
+  exports: [
+    ScrollDownComponent,
+    CardComponent,
+    ProjectCardComponent,
+    FooterComponent,
+  ],
 })
 export class SharedModule {}
