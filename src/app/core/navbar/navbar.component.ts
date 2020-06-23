7214e9a894dd78c32ddb2f6465194c4d69200027
Daniel <darknessbg@windowslive.com>
2020-06-23 17:15:53 +0300

    burger menu works now, fixed some responsive bugs

--- src/app/core/navbar/navbar.component.ts
+++ src/app/core/navbar/navbar.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-navbar',
-  templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  selector: "app-navbar",
+  templateUrl: "./navbar.component.html",
+  styleUrls: ["./navbar.component.css"],
 })
 export class NavbarComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit(): void {}
+  handleMenuClick(menu: HTMLDivElement, burger: HTMLAnchorElement) {
+    menu.classList.toggle("is-active");
+    burger.classList.toggle("is-active");
   }
-
 }
